351620a52747442147ddcf7e3d4db82616f6649b
shyl1 <sohaylae615@gmail.com>
2025-06-25 00:39:03 +0300

    add sound to butterfly-effect page

--- src/feedback/ButterflyEffect.tsx
+++ src/feedback/ButterflyEffect.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react"
 
 export default function ButterflyEffect() {
+  // add sound effect
+  useEffect(()=> {
+    const butterflyAudio = new Audio("/sounds/This-Action-Will-Have-Consequences.mp3");
+    butterflyAudio.loop = true; // loop the sound effect
+    butterflyAudio.play().catch((error) => {
+      console.error("Error playing audio:", error);
+    });
+
+    // clean up function to stop the audio when the component unmounts
+    return () => {
+      butterflyAudio.pause();
+      butterflyAudio.currentTime = 0;
+    };
+  }, []);
+
+
   return (
     <section className="bg-[url('/images/rewind-bg.png')] bg-cover bg-center h-screen w-screen flex max-md:items-center  justify-center">
 
