216ad51dd56939dd240f6b3b34576817c078e914
shyl1 <sohaylae615@gmail.com>
2026-01-01 20:55:37 +0200

    ts

--- src/pages/books/AboutBook/Book.tsx
+++ src/pages/books/AboutBook/Book.tsx
@@ -7,64 +7,64 @@ import Page1 from "./Page1";
 
 export default function AboutBook() {
     usePageLoading();
-    const bookRef = useRef<HTMLFlipBook>(null);
+    const bookRef = useRef<typeof HTMLFlipBook>(null);
     return (
         <>
-        <section className="hidden h-screen  md:flex flex-col justify-center items-center bg-[url('/images/multiple-choices-bg.png')] bg-center bg-cover"
-        >
-            <HTMLFlipBook
-style={{}}           
-  size="fixed" 
-                ref={bookRef}
-                className="shadow-2xl rounded"
-                showCover={false}
-                maxShadowOpacity={0.5}
-                useMouseEvents={true}
-                startPage={0}
-                autoSize={true}
-                 width={600}   
-          height={600}  
-          mobileScrollSupport={true}
+            <section className="hidden h-screen  md:flex flex-col justify-center items-center bg-[url('/images/multiple-choices-bg.png')] bg-center bg-cover"
             >
-                <div>
-                    <Page1 />
-                </div>
+                <HTMLFlipBook
+                    style={{}}
+                    size="fixed"
+                    ref={bookRef}
+                    className="shadow-2xl rounded"
+                    showCover={false}
+                    maxShadowOpacity={0.5}
+                    useMouseEvents={true}
+                    startPage={0}
+                    autoSize={true}
+                    width={600}
+                    height={600}
+                    mobileScrollSupport={true}
+                >
+                    <div>
+                        <Page1 />
+                    </div>
 
-                <div>
-                    <Page2 />
-                </div>
-            </HTMLFlipBook>
+                    <div>
+                        <Page2 />
+                    </div>
+                </HTMLFlipBook>
 
-        </section>
+            </section>
 
-        <section className="flex h-screen md:hidden flex-col justify-center items-center bg-[url('/images/multiple-choices-bg.png')] bg-center bg-cover"
-        >
-            <HTMLFlipBook
-                style={{}}           // <-- REQUIRED BY TS
-                 size="fixed" 
-                ref={bookRef}
-                className="shadow-2xl rounded"
-                showCover={false}
-                maxShadowOpacity={0.5}
-                useMouseEvents={true}
-                startPage={0}
-                autoSize={true}
-                 width={350}   
-          height={600}  
-         
-          mobileScrollSupport={true}
+            <section className="flex h-screen md:hidden flex-col justify-center items-center bg-[url('/images/multiple-choices-bg.png')] bg-center bg-cover"
             >
-                <div>
-                    <Page1 />
-                </div>
+                <HTMLFlipBook
+                    style={{}}           // <-- REQUIRED BY TS
+                    size="fixed"
+                    ref={bookRef}
+                    className="shadow-2xl rounded"
+                    showCover={false}
+                    maxShadowOpacity={0.5}
+                    useMouseEvents={true}
+                    startPage={0}
+                    autoSize={true}
+                    width={350}
+                    height={600}
 
-                <div>
-                    <Page2 />
-                </div>
-            </HTMLFlipBook>
+                    mobileScrollSupport={true}
+                >
+                    <div>
+                        <Page1 />
+                    </div>
 
-        </section>
-        
+                    <div>
+                        <Page2 />
+                    </div>
+                </HTMLFlipBook>
+
+            </section>
+
         </>
     );
 }
